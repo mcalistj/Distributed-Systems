3d2ec88e0319795cf77188532315028d665aec04
John McAlister <mcalistj@tcd.ie>
2015-10-16 17:10:36 +0100

    Kill service functionality

--- Lab 2 - pure ruby/simple_client.rb
+++ Lab 2 - pure ruby/simple_client.rb
@@ -4,16 +4,27 @@ hostname = '127.0.0.1'
 port = 8000
 threads = []
 
-20.times do |i|
-  t = Thread.new do
-    puts "Client Connection #{i}"
-    s = TCPSocket.open(hostname, port)
-    while true
-      while line = s.gets # Read lines from socket
-        puts line         # and print them
-      end
+def test_kill_service(hostname, port, thread_num)         # Function to test behaviour of server to receiving
+  puts "Client connection attempt number #{thread_num}"   # "KILL_SERVICE\n" if sent multiple times
+  begin
+    connection = TCPSocket.open(hostname, port)
+    connection.print("KILL_SERVICE\n")
+    response = connection.gets                            # Establishing a type of protocol
+    puts response
+    if connection && response then                        # Debugging line to test whether the server gets terminated
+      puts "Succeeded #{thread_num}"
+    else
+      puts "CONNECTION #{thread_num} Failed"
     end
-    #s.close             # close socket when done
+    connection.close                                      # close socket when done
+  rescue
+    puts "Connection #{thread_num} Failed"
+  end
+end
+
+100.times do |i|
+  t = Thread.new do
+    test_kill_service(hostname, port, i)
   end
   threads.push(t)
 end
--- Lab 2 - pure ruby/threadpoolingserver.rb
+++ Lab 2 - pure ruby/threadpoolingserver.rb
@@ -6,7 +6,7 @@ class ThreadPool
     @size = size                      # Number of threads 
     @connections = Queue.new          # Initialise instance of 'Queue'
     @pool = Array.new(@size) do |i|
-      Thread.new do
+      t = Thread.new do
         Thread.current[:id] = i
         catch(:exit) do
           loop do
@@ -22,18 +22,13 @@ class ThreadPool
     @connections << [block, args]      # << Alias of push
   end
   
-  def kill_service()
-    puts @pool.length
-    delete(@pool)
-    delete(@connections)
-    __END__
-    puts "Hello"
-    #abort("Message contains KILL_SERVICE\n")
+  def kill_service(client, server)
+    @connections.clear
+    client.close
+    server.close
+    #delete(pool)
   end
-  
-  def size_of_pool()
-    return @pool.length
-  end
+
 end
 
 tp = ThreadPool.new(10)
@@ -44,12 +39,20 @@ port = 8000
 
 server = TCPServer.open(hostname, port)     # Socket to listen on port 8000
 loop {                                      # Servers run forever
-  client = server.accept                    # Wait for a client to connect
-  tp.schedule do client                     # The following block of code gets put in p.connections = our threadpool work
-    client.puts(Time.now.ctime)             # Send the time to the client
-    connectionNumber += 1
-    client.puts "Thread number #{connectionNumber} by thread #{Thread.current[:id]}"
-    client.puts "Closing the connection. Bye!"
-    client.close                            # Disconnect from the client
+  begin 
+    client = server.accept                    # Wait for a client to connect
+    tp.schedule do client                     # The following block of code gets put in p.connections = our threadpool work
+      sleep rand(2)                           # Do some work. Introducing the random time period simulates a scenario where "KILL_SERVICE\n" 
+                                              # can kill the other working threads because they no longer appear to exit at the same time
+      client.print "Connection Established"   # Need to incorporated some type of protocol
+      line = client.gets                      # Read lines from socket
+      puts line                               # and print them
+      if line.include? "KILL_SERVICE\n" then
+        tp.kill_service(client, server)
+      end
+      client.close                            # Disconnect from the client
+    end
+  rescue
+    break
   end
 }
