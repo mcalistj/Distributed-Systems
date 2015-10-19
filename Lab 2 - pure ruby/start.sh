34e830dc7c880aa600e31cf1a2de0c9e499dfd71
John <mcalistj@tcd.ie>
2015-10-19 14:02:37 +0100

    Amend directory in start script

--- Lab 2 - pure ruby/start.sh
+++ Lab 2 - pure ruby/start.sh
@@ -6,6 +6,6 @@ echo $1' -> echo $1'
 
 echo "Starting up server listening on" $1
 
-ruby ~/Downloads/Distributed\ Systems/Distributed-Systems/Lab\ 2\ -\ pure\ ruby/threadpoolingserver.rb $1
+ruby ~/Downloads/Distributed-Systems/Lab\ 2\ -\ pure\ ruby/threadpoolingserver.rb $1
 
 echo "Exiting bash script"
