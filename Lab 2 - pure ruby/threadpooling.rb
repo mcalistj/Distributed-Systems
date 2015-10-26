1d7454428dc8381a091463f640c8eebb8de3a8e3
John McAlister <mcalistj@tcd.ie>
2015-10-26 21:27:56 +0000

    Tested against 'official client'

---
+++ Lab 2 - pure ruby/threadpooling.rb
@@ -0,0 +1,45 @@
+require 'socket'               # Get sockets from stdlib
+require 'thread'               # In order to operate pooling
+
+class Pool
+
+  def initialize(size)
+    @size = size
+    @jobs = Queue.new
+    @pool = Array.new(@size) do |i|
+      Thread.new do
+        Thread.current[:id] = i
+        catch(:exit) do
+          loop do
+            job, args = @jobs.pop
+            job.call(*args)
+          end
+        end
+      end
+    end
+  end
+  
+  def schedule(*args, &block)
+    @jobs << [block, args]      # << Alias of push
+  end
+  
+  def shutdown
+    @size.times do
+        schedule { throw :exit }
+      end
+    @pool.map(&:join)
+  end
+end
+  
+if $0 == __FILE__
+  p = Pool.new(10)
+
+  20.times do |i|
+    p.schedule do
+      sleep rand(4) + 2
+      puts "Job #{i} finished by thread #{Thread.current[:id]}"
+    end
+  end
+  
+  at_exit { p.shutdown }
+end
